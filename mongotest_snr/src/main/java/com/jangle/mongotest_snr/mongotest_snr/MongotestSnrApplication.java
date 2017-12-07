1acb9c1be5fef1f77b3c54fe6c9c285b57edbc1e
Şener KÖRÜKMEZ <noxsener@gmail.com>
2017-12-07 14:43:43 +0300

    First commit

---
+++ mongotest_snr/src/main/java/com/jangle/mongotest_snr/mongotest_snr/MongotestSnrApplication.java
@@ -0,0 +1,12 @@
+package com.jangle.mongotest_snr.mongotest_snr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongotestSnrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongotestSnrApplication.class, args);
+	}
+}
